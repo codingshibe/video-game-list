5b14d3bee494c638c220e297ae455ee9dbd0fa70
matchashiba <recr38@gmail.com>
2020-01-07 00:11:16 +0000

    Created initial GradeForm component

---
+++ client/components/GradeForm.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class GradeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      course: '',
+      grade: ''
+    };
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default GradeForm;
