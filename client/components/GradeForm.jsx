6f2dfc37dbbb3843e00289febc40a3691c9294ce
matchashiba <recr38@gmail.com>
2020-01-07 18:42:56 +0000

    Completed form layout of GradeForm

--- client/components/GradeForm.jsx
+++ client/components/GradeForm.jsx
@@ -9,6 +9,21 @@ class GradeForm extends React.Component {
       grade: ''
 
     };
+    this.handleNameInput = this.handleNameInput.bind(this);
+    this.handleCourseInput = this.handleCourseInput.bind(this);
+    this.handleGradeInput = this.handleGradeInput.bind(this);
+  }
+
+  handleNameInput(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleCourseInput(event) {
+    this.setState({ course: event.target.value });
+  }
+
+  handleGradeInput(event) {
+    this.setState({ grade: event.target.value });
   }
 
   render() {
@@ -20,6 +35,7 @@ class GradeForm extends React.Component {
               <i className='fas fa-user' />
             </span>
           </div>
+          <input type='text' className='form-control' placeholder='Student Name' onChange={this.handleNameInput}></input>
         </div>
         <div className='input-group mb-3'>
           <div className='input-group-prepend'>
@@ -27,6 +43,7 @@ class GradeForm extends React.Component {
               <i className='fas fa-list-alt' />
             </span>
           </div>
+          <input type='text' className='form-control' placeholder='Course' onChange={this.handleCourseInput}></input>
         </div>
         <div className='input-group mb-3'>
           <div className='input-group-prepend'>
@@ -34,7 +51,10 @@ class GradeForm extends React.Component {
               <i className='fas fa-graduation-cap' />
             </span>
           </div>
+          <input type='number' className='form-control' placeholder='Grade' onChange={this.handleGradeInput}></input>
         </div>
+        <button type='submit' className='btn btn-success' id='addButton'>Add</button>
+        <button type='button' className='btn btn-light' id='cancelButton'>Cancel</button>
       </div>
     );
   }
