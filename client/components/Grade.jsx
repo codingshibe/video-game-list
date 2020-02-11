925ba8d1200a5a248d62d02dc966610b213add0e
matchashiba <recr38@gmail.com>
2020-02-11 21:42:48 +0000

    Converted references from grades to games in Grade.jsx and GradeTable.jsx

--- client/components/Grade.jsx
+++ client/components/Grade.jsx
@@ -4,12 +4,12 @@ class Grade extends React.Component {
   render() {
     return (
       <tr>
-        <td>{this.props.name}</td>
-        <td>{this.props.course}</td>
-        <td>{this.props.grade}</td>
+        <td>{this.props.title}</td>
+        <td>{this.props.platform}</td>
+        <td>{this.props.price}</td>
         <td><button type='button' className='btn btn-danger btn-sm'
-          onClick={() => this.props.onDelete(this.props.gradeId)}>
-          Delete</button> <button type='button' className='btn btn-warning btn-sm' onClick={() => this.props.populateForm(this.props.gradeId)}>Update</button></td>
+          onClick={() => this.props.onDelete(this.props.gameId)}>
+          Delete</button> <button type='button' className='btn btn-warning btn-sm' onClick={() => this.props.populateForm(this.props.gameId)}>Update</button></td>
       </tr>
     );
   }
--- client/components/GradeTable.jsx
+++ client/components/GradeTable.jsx
@@ -3,7 +3,7 @@ import Grade from './Grade';
 
 class GradeTable extends React.Component {
 
-  generateGradeRows() {
+  generateGameRows() {
     const gamesList = this.props.games;
     const gameRows = gamesList.map(game => {
       return <Grade key={game.gameId} gameId={game.gameId} title={game.title} platform={game.platform} price={game.price} onDelete={this.props.deleteMethod} populateForm={this.props.populateForm}/>;
@@ -13,21 +13,21 @@ class GradeTable extends React.Component {
   }
 
   checkForEmptyData() {
-    const gradesList = this.props.grades;
-    if (gradesList.length === 0) {
-      return <div>There are no records to display</div>;
+    const gamesList = this.props.games;
+    if (gamesList.length === 0) {
+      return <div>There are no games to display</div>;
     }
     return (
       <table className='table table-striped'>
         <thead>
           <tr>
-            <th scope='col'>Student Name</th>
-            <th scope='col'>Course</th>
-            <th scope='col'>Grade</th>
+            <th scope='col'>Title</th>
+            <th scope='col'>Platform</th>
+            <th scope='col'>Price</th>
           </tr>
         </thead>
         <tbody>
-          {this.generateGradeRows()}
+          {this.generateGameRows()}
         </tbody>
       </table>
     );
