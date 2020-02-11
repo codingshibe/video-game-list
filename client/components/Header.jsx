327d7f48d008cf6300fc6f40c1b43d7141e6472b
matchashiba <recr38@gmail.com>
2020-02-11 22:26:49 +0000

    Changed font for Header.jsx h2 tag Changed color of h2 tag in Header.jsx

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -5,7 +5,7 @@ function Header(props) {
     <div className='container'>
       <div className='row'>
         <div className='col-xs-12 col-sm-12 col-md-9'>
-          <h2>Video Games List</h2>
+          <h2 className='headline'>Video Games List</h2>
         </div>
         <div className='col-xs-12 col-sm-12 col-md-3'><h4>Average Price <span className='badge badge-secondary'>{props.average}</span></h4></div>
       </div>
