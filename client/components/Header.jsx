640577f907e4609000c761bc6b97bf01035330bf
matchashiba <recr38@gmail.com>
2020-02-12 00:03:50 +0000

    Changed color scheme to dark Added outline to Header.jsx items Changed Add and Update button colors

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -7,7 +7,7 @@ function Header(props) {
         <div className='col-xs-12 col-sm-12 col-md-9'>
           <h2 className='headline'>Video Games List</h2>
         </div>
-        <div className='col-xs-12 col-sm-12 col-md-3'><h4>Average Price <span className='badge badge-secondary'>{props.average}</span></h4></div>
+        <div className='col-xs-12 col-sm-12 col-md-3 average'><h4>Average Price <span className='badge badge-light'><i className='fas fa-dollar-sign'/> {props.average}</span></h4></div>
       </div>
     </div>
   );
